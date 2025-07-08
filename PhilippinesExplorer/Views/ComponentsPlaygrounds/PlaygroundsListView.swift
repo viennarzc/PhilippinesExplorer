7032365084bb65450fe84bf1182c0400ad38a55f
Viennarz Curtiz <viennarzc@gmail.com>
2025-07-08 14:45:55 +0800

    Create enum for destinations in PlaygroundsListView, embed Navigation Split to TabView

--- PhilippinesExplorer/Views/ComponentsPlaygrounds/PlaygroundsListView.swift
+++ PhilippinesExplorer/Views/ComponentsPlaygrounds/PlaygroundsListView.swift
@@ -7,71 +7,91 @@
 
 import SwiftUI
 
+enum PlaygroundsListDestination: CaseIterable, Equatable, Hashable, Identifiable {
+    case togglesView
+    case colorPickerView
+    case buttonsView
+    case datePickerView
+    case formsView
+    case listView
+    case gaugeView
+    case menuView
+    case pickerView
+    case playgroundView
+    case playgroundView2
+    case presentationView
+    case progressView
+    case sliderView
+    case stepperView
+    case textFieldView
+    case playgroundsListView
+
+    var id: String {
+        String(describing: self)
+    }
+
+    var title: String {
+        switch self {
+        case .playgroundsListView:
+            return "Playgrounds"
+        default:
+            return String(describing: self)
+        }
+    }
+
+    @MainActor @ViewBuilder func viewForPage() -> some View {
+        switch self {
+        case .playgroundsListView:
+            PlaygroundsListView()
+        case .togglesView:
+            ComponentControlsToggleView()
+        case .colorPickerView:
+            ComponentsPickerView()
+        case .buttonsView:
+            ComponentsControlsButtonView()
+
+        case .datePickerView:
+            ComponentsDatePickerView()
+        case .formsView:
+            ComponentsExploreFormsView()
+        case .listView:
+            ComponentsExploreListView()
+        case .gaugeView:
+            ComponentsGaugeView()
+        case .menuView:
+            ComponentsMenuView()
+        case .pickerView:
+            ComponentsPickerView()
+        case .playgroundView:
+            ComponentsPlaygroundView()
+        case .playgroundView2:
+            ComponentsPlaygroundView2()
+        case .presentationView:
+            ComponentsPresentationsView()
+        case .progressView:
+            ComponentsProgressView()
+        case .sliderView:
+            ComponentsSliderView()
+        case .stepperView:
+            ComponentsStepperView()
+        case .textFieldView:
+            ComponentsTextFieldView()
+        }
+    }
+}
+
 struct PlaygroundsListView: View {
     var body: some View {
         NavigationStack {
             List {
-                NavigationLink {
-                    ComponentsPlaygroundView()
-                } label: {
-                    Text("Components Playground 1")
+                ForEach(PlaygroundsListDestination.allCases, id: \.self) { destination in
+                    NavigationLink {
+                        destination.viewForPage()
+                    } label: {
+                        Text(destination.title)
+                    }
+                    
                 }
-                
-                NavigationLink {
-                    ComponentsPlaygroundView2()
-                } label: {
-                    Text("Components Playground 2")
-                }
-                
-                NavigationLink {
-                    ComponentsExploreListView()
-                } label: {
-                    Text("Lists")
-                }
-                
-                NavigationLink {
-                    ComponentsExploreFormsView()
-                } label: {
-                    Text("Forms")
-                }
-                
-                NavigationLink {
-                    ComponentControlsToggleView()
-                } label: {
-                    Text("Toggles")
-                }
-                
-                NavigationLink {
-                    ComponentsControlsButtonView()
-                } label: {
-                    Text("Buttons")
-                }
-                
-                NavigationLink {
-                    ComponentsMenuView()
-                } label: {
-                    Text("Menu")
-                }
-                
-                NavigationLink {
-                    ComponentsPickerView()
-                } label: {
-                    Text("Picker")
-                }
-                
-                NavigationLink {
-                    ComponentsSliderView()
-                } label: {
-                    Text("Slider")
-                }
-                
-                NavigationLink {
-                    ComponentsDatePickerView()
-                } label: {
-                    Text("Date Picker")
-                }
-                
-                
             }
             .navigationTitle("Playgrounds List")
         }
--- PhilippinesExplorer/Views/PhilippinesExplorerSplitView.swift
+++ PhilippinesExplorer/Views/PhilippinesExplorerSplitView.swift
@@ -8,47 +8,105 @@ import SwiftUI
 
 /// A view that establishes split view navigation for the app.
 struct PhilippinesExplorerSplitView: View {
-    
+    enum AppTab {
+        case one
+        case two
+        case three
+        case four
+        case five
+        case six
+    }
+
     @State private var preferredColumn: NavigationSplitViewColumn = .detail
     @State private var path: NavigationPath = NavigationPath()
     @State private var selectedIsland: IslandGroup? = nil
     @State private var isInspectorPresented: Bool = false
     @State private var searchString: String = ""
+    @State private var selection: AppTab
+        = .one
 
     var body: some View {
-        NavigationSplitView(preferredCompactColumn: $preferredColumn) {
-            List {
-                Section {
-                    ForEach(NavigationOptions.mainPages) { page in
-                        NavigationLink(value: page) {
-                            Label(page.name, systemImage: page.symbolName)
+        TabView(selection: $selection) {
+            Tab(
+                "Tab one",
+                systemImage: "house",
+                value: .one
+            ) {
+                NavigationSplitView(preferredCompactColumn: $preferredColumn) {
+                    List {
+                        Section {
+                            ForEach(NavigationOptions.mainPages) { page in
+                                NavigationLink(value: page) {
+                                    Label(page.name, systemImage: page.symbolName)
+                                }
+                            }
                         }
                     }
+                    .navigationDestination(for: NavigationOptions.self) { page in
+                        NavigationStack(path: $path) {
+                            page.viewForPage()
+                        }
+                        .navigationDestination(for: IslandGroup.self) { island in
+                            IslandGroupView(islandGroup: island)
+                        }
+                        .navigationDestination(for: Region.self) { _ in
+                        }
+                        //                .showsBadges()
+                    }
+                    .frame(minWidth: 150)
+                    .navigationTitle(Text("Philippines Explorer"))
+                } detail: {
+                    NavigationStack(path: $path) {
+                        NavigationOptions.majorIslandGroups.viewForPage()
+                    }
+                    .navigationDestination(for: IslandGroup.self) { _ in
+                    }
+                    //            .showsBadges()
                 }
             }
-            .navigationDestination(for: NavigationOptions.self) { page in
-                NavigationStack(path: $path) {
-                    page.viewForPage()
-                }
-                .navigationDestination(for: IslandGroup.self) { island in
-                    IslandGroupView(islandGroup: island)
-                }
-                .navigationDestination(for: Region.self) { collection in
-//                    CollectionDetailView(collection: collection)
-                }
-//                .showsBadges()
+            
+            Tab(
+                "Tab 2",
+                systemImage: "network",
+                value: .two
+            ) {
+                Text("Some Content")
             }
-            .frame(minWidth: 150)
-            .navigationTitle(Text("Philippines Explorer"))
-        } detail: {
-            NavigationStack(path: $path) {
-                NavigationOptions.majorIslandGroups.viewForPage()
+            
+            Tab(
+                "Tab 3",
+                systemImage: "bolt.circle",
+                value: .three
+            ) {
+                Text("Some Content 2")
             }
-            .navigationDestination(for: IslandGroup.self) { island in
-//                LandmarkDetailView(landmark: landmark)
+            
+            Tab(
+                "Tab 5",
+                systemImage: "tray.2",
+                value: .five
+            ) {
+                Text("Some Much content")
             }
-//            .showsBadges()
+            
+            Tab(
+                "Tab 6",
+                systemImage: "calendar.badge.clock",
+                value: .six
+            ) {
+                Text("Some Much content")
+            }
+            
+            Tab(
+                "Search",
+                systemImage: "magnifyingglass",
+                value: .four,
+                role: .search) {
+                    Text("Some content for search")
+                }
         }
+        .tint(.pink)
+        .tabViewStyle(.tabBarOnly)
         // Adds global search, where the system positions the search bar automatically
         // in content views.
         .searchable(text: $searchString, prompt: "Search")
